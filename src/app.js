4bdef82f07f3f63dc41e2265de21b707ebc7b4fc
madeinchema <chemavg1@gmail.com>
2020-11-06 18:40:52 +0100

    Implemented routes into the App using a routes file with react-router-dom.

--- src/app.js
+++ src/app.js
@@ -1,14 +1,28 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { JumbotronContainer } from './containers/jumbotron';
 import { FooterContainer } from './containers/footer';
 import { FaqsContainer } from './containers/faqs';
+import * as ROUTES from './constants/routes';
+import { Home, Browse, Signin, Signup } from './pages';
 
 export default function App() {
   return (
-    <>
-      <FaqsContainer />
-      <JumbotronContainer />
-      <FooterContainer />
-    </>
+    <Router>
+      <Switch>
+        <Route exact path={ROUTES.HOME}>
+          <Home />
+        </Route>
+        <Route path={ROUTES.BROWSE}>
+          <Browse />
+        </Route>
+        <Route path={ROUTES.SIGN_IN}>
+          <Signin />
+        </Route>
+        <Route path={ROUTES.SIGN_UP}>
+          <Signup />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
